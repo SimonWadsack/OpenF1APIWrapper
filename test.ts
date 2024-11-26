a91155a51863a8ef069ee98b1ed7dd5a0304c2e7
Simon Wadsack <dev.simon@wadsack.net>
2024-11-26 15:53:00 +0100

    Added more test information

--- test.ts
+++ test.ts
@@ -1,5 +1,15 @@
 import { getMeetingsForQuery, getMeetingsForYear } from './apiWrapper';
 import { Meeting } from './apiClasses';
 
+
+//get the meetings with .then
 getMeetingsForYear(2023)
     .then((meetings: Meeting[]) => { console.log(meetings); });
+
+//get the meetings with an async function
+async function test(){
+    const meetings = await getMeetingsForYear(2023);
+    console.log(meetings);
+}
+
+test();
